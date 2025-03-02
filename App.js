fd3c398616c60fac1fc044c5a3338b55154c3cec
Angelo <gabriel_propio95@hotmail.com>
2025-03-02 15:03:15 -0400

    archivos necesarios para usar tailwind en react antive

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+
+import './global.css'
 
 export default function App() {
+  const getAlert = ()=>{
+    Alert.alert('Hola mundo');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-lime-400 text-4xl">Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500">Segundo texto de prueba</Text>
+      <View className="mt-6 rounded-lg p-8 bg-indigo-500 shadow-lg shadow-indigo-500/50">
+        <Text className="text-white">Dale click</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
